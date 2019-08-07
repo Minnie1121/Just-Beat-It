9ae39a14d9b277193fbfdb60139323db5ea9a1d9
Minnie Ren <39520129+Minnie1121@users.noreply.github.com>
2019-08-07 12:04:59 +0800

    Add files via upload

--- circleClass.py
+++ circleClass.py
@@ -2,6 +2,13 @@
 
 import random
 
+def drawGrid(canvas, data):
+	for row in range(5):
+		for col in range(5):
+			canvas.create_rectangle(40+col*data.cellW, 60+row*data.cellH,
+									40+(col+1)*data.cellW, 60+(row+1)*data.cellH,
+									width = 2)
+
 class Circle(object):
 	def __init__(self, time, color="blue"):
 		self.r = 20
@@ -13,8 +20,8 @@ class Circle(object):
 			for col in range(5):
 				if data.grid[row][col] == False:
 					data.grid[row][col] = True
-					cx = 40 + col*data.cellW
-					cy = 60 + row*data.cellH
+					cx = 40 + col*data.cellW + 1/2*data.cellW
+					cy = 60 + row*data.cellH + 1/2*data.cellH
 					canvas.create_oval(cx-self.r, cy-self.r,
 									   cx+self.r, cy+self.r,
 									   fill = self.color)
