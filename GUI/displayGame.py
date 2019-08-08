35bbda7c845024e0b00fe30ac1d4f4813a8072f3
Minnie Ren <39520129+Minnie1121@users.noreply.github.com>
2019-08-09 01:26:40 +0800

    Add files via upload

--- GUI/displayGame.py
+++ GUI/displayGame.py
@@ -24,6 +24,7 @@ def init(data):
 	data.press = 0		# at what time the key was pressed (time())
 	data.songStart = 0	# when did the song start to play
 	data.isPressed = False
+	data.isClicked = False
 	data.songTimer = 0
 	data.startGame = False
 
@@ -38,7 +39,7 @@ def init(data):
 	data.circles = []
 	data.lastBeat = None
 	data.timer = 0
-	data.levels = []	# how good the user clicks; tuple (level, circle)
+	data.levels = []	# Perfect, Good, OK, Miss
 
 	data.score = 0
 
@@ -51,27 +52,37 @@ def mousePressed(event, data):
 	if (data.mode == "selectSong"): selectSongMousePressed(event, data)
 	elif (data.mode == "tapBeats"): tapBeatsMousePressed(event, data)
 	elif (data.mode == "playGame"): playGameMousePressed(event, data)
+	elif (data.mode == "gameOver"): gameOverMousePressed(event, data)
+
+def mouseRelease(event, data):
+	if (data.mode == "selectSong"): selectSongMouseRelease(event, data)
+	elif (data.mode == "tapBeats"): tapBeatsMouseRelease(event, data)
+	elif (data.mode == "playGame"): playGameMouseRelease(event, data)
+	elif (data.mode == "gameOver"): gameOverMouseRelease(event, data)
 
 def keyPressed(event, data):
 	if (data.mode == "selectSong"): selectSongKeyPressed(event, data)
 	elif (data.mode == "tapBeats"): tapBeatsKeyPressed(event, data)
 	elif (data.mode == "playGame"): playGameKeyPressed(event, data)
+	elif (data.mode == "gameOver"): gameOverKeyPressed(event, data)
 
 def keyRelease(event, data):
 	if (data.mode == "selectSong"): selectSongKeyRelease(event, data)
 	elif (data.mode == "tapBeats"): tapBeatsKeyRelease(event, data)
 	elif (data.mode == "playGame"): playGameKeyRelease(event, data)
+	elif (data.mode == "gameOver"): gameOverKeyRelease(event, data)
 
 def timerFired(data):
 	if (data.mode == "selectSongTimerFired"): selectSongTimerFired(data)
 	elif (data.mode == "tapBeats"): tapBeatsTimerFired(data)
 	elif (data.mode == "playGame"): playGameTimerFired(data)
-
+	elif (data.mode == "gameOver"): gameOverTimerFired(data)
 
 def redrawAll(canvas, data):
 	if (data.mode == "selectSong"): selectSongRedrawAll(canvas, data)
 	elif (data.mode == "tapBeats"): tapBeatsRedrawAll(canvas, data)
 	elif (data.mode == "playGame"): playGameRedrawAll(canvas, data)
+	elif (data.mode == "gameOver"): gameOverRedrawAll(canvas, data)
 
 
 
@@ -81,10 +92,15 @@ def redrawAll(canvas, data):
 
 def browseFile():
 	global fileName
-	fileName = filedialog.askopenfilename(filetypes = [("WAV file", "*.wav")])
+	fileName = filedialog.askopenfilename(filetypes=[("All Files","*.*"),
+													 ("WAV file", "*.wav"),
+													 ("mp3 file", "*.mp3")])
 
 
 def selectSongMousePressed(event, data):
+	pass
+
+def selectSongMouseRelease(event, data):
 	pass
 
 def selectSongKeyPressed(event, data):
@@ -114,6 +130,9 @@ def selectSongRedrawAll(canvas, data):
 def tapBeatsMousePressed(event, data):
 	pass
 
+def tapBeatsMouseRelease(event, data):
+	pass
+
 def tapBeatsKeyPressed(event, data):
 	if not mixer.music.get_busy():
 		if event.keysym == "m":
@@ -132,7 +151,7 @@ def tapBeatsKeyRelease(event, data):
 		now = time.time()	# sec
 		timestamp = (data.press-data.songStart, now-data.songStart)
 		data.beats.append(timestamp)
-		# print(timestamp)
+		print(timestamp)
 	data.isPressed = False
 
 def tapBeatsTimerFired(data):
@@ -154,37 +173,55 @@ def tapBeatsRedrawAll(canvas, data):
 def almostEqual(x, y):
 	return 0 <= abs(x-y) <= 0.1		# 0.1 sec
 
-def clickLevel(clickTime, circle):
+def clickLevel(data, clickTime, circle):
+	# releaseTime will be the clickTime for longCir
 	diff = clickTime - circle.timestamp[0]
 	length = circle.timestamp[1] - circle.timestamp[0]
-	if 0 <= diff <= 0.7:
-		data.score += 10
-		return "Perfect"
-	elif diff <= 1:
-		data.score += 5
-		return "Good"
-	elif diff <= max(length, 2):
-		data.score += 3
-		return "OK"
+	if circle.isLongCir():
+		if 0 <= diff <= 0.7:
+			data.score += 10
+			return "Perfect"
+		elif diff <= 1:
+			data.score += 5
+			return "Good"
+		elif diff <= max(length, 3):
+			data.score += 3
+			return "OK"
+		else:
+			return "Miss"
 	else:
-		return "Miss"
+		if 0 <= diff <= 0.7:
+			data.score += 10
+			return "Perfect"
+		elif diff <= 1:
+			data.score += 5
+			return "Good"
+		elif diff <= max(length, 2):
+			data.score += 3
+			return "OK"
+		else:
+			return "Miss"
 
 
 def playGameMousePressed(event, data):
+	# deal with normal circles in mouse press
+	# only change the state of data.isClicked for long circles here
 	if not data.startGame:
 		if 0 < event.x < data.width and 0 < event.y < data.height:
 			data.startGame = True
 			data.songTimer = time.time()	# sec; when the music (game) starts
 			mixer.music.load(fileName)
 			mixer.music.play()
-	else:		# game started
+	elif mixer.music.get_busy():
 		toRemove = []
 		for circle in data.circles:
 			if circle.clickedIn(event.x, event.y):
-				toRemove.append(circle)
-				clickTime = time.time() - data.songTimer
-				level = clickLevel(clickTime, circle)
-				data.levels.append((level, circle))
+				data.isClicked = True
+				if not circle.isLongCir():
+					clickTime = time.time() - data.songTimer
+					level = clickLevel(data, clickTime, circle)
+					data.levels.append((level, circle))
+					toRemove.append(circle)
 		remaining = []
 		for circle in data.circles:
 			if circle not in toRemove:
@@ -192,6 +229,23 @@ def playGameMousePressed(event, data):
 		data.circles = remaining
 
 
+def playGameMouseRelease(event, data):
+	# deal with long circles in mouse release
+	if data.isClicked and mixer.music.get_busy():
+		toRemove = []
+		for circle in data.circles:
+			if circle.isLongCir():
+				if circle.releasedIn(event.x, event.y):
+					releaseTime = time.time() - data.songTimer
+					level = clickLevel(data, releaseTime, circle)
+					data.levels.append((level, circle))
+					toRemove.append(circle)
+		remaining = []
+		for circle in data.circles:
+			if circle not in toRemove:
+				remaining.append(circle)
+		data.circles = remaining
+	data.isClicked = False
 
 
 def playGameKeyPressed(event, data):
@@ -202,59 +256,87 @@ def playGameKeyRelease(event, data):
 
 def playGameTimerFired(data):
 	if data.startGame:
-		data.timer += 1
-		for circle in data.circles:
-			circle.timer += 1
+		if not mixer.music.get_busy():
+			data.mode = "gameOver"
+		else:
+			data.timer += 1
+			for circle in data.circles:
+				circle.timer += 1
 
-		for beat in data.beats:
-			if almostEqual(data.timer/10, beat[0]) and beat != data.lastBeat:
-				data.circles.append(Circle(beat, 0))
-				data.lastBeat = beat 	# avoid the extra timerFired calls
-				break
-
-		toRemove = []
-		for circle in data.circles:
-			length = circle.timestamp[1] - circle.timestamp[0]
-			if length < 1:	# key press, circle stays for 1 sec
-				if almostEqual(data.timer/10, circle.timestamp[0]+1.5):
-					toRemove.append(circle)
-			else:		# key hold, circle stays until key release or 2 sec
-				if almostEqual(data.timer/10, 
-							   circle.timestamp[0]+max(2, length)):
-					toRemove.append(circle)
-
-		remaining = []
-		for circle in data.circles:
-			if circle not in toRemove:
-				remaining.append(circle)
-		data.circles = remaining
+			for beat in data.beats:
+				if (almostEqual(data.timer/10, beat[0]) 
+						and beat != data.lastBeat):
+					data.circles.append(Circle(beat, 0))
+					data.lastBeat = beat 	# avoid the extra timerFired calls
+					break
+			toRemove = []
+			for circle in data.circles:
+				length = circle.timestamp[1] - circle.timestamp[0]
+				if length < 1:	# key press
+					if almostEqual(data.timer/10, 
+								   circle.timestamp[0]+2):
+						toRemove.append(circle)
+				else:			# key hold
+					if almostEqual(data.timer/10, 
+								   circle.timestamp[0]+max(3, length)):
+						toRemove.append(circle)
+			remaining = []
+			for circle in data.circles:
+				if circle not in toRemove:
+					remaining.append(circle)
+			data.circles = remaining
 
 
 def playGameRedrawAll(canvas, data):
-	drawGrid(canvas, data)
 	canvas.create_text(data.width/2, 20, font="Arial 20",
-					   text="click anywhere to start game")
-	if data.startGame and mixer.music.get_busy():
+					   text="click anywhere to start game\tScore: %d" 
+					   									% data.score)
+	if mixer.music.get_busy():
 		data.grid = [ [False] * 5 for row in range(5) ]
 		for circle in data.circles:
 			circle.draw(canvas, data)
 
 		while len(data.levels) > 0:
 			nextLevel = data.levels[0]	
-				# nextLevel[0]: text; nextLevel[1]: circle
-			canvas.create_text(nextLevel[1].cx, nextLevel[1].cy-20,
-							   text = nextLevel[0], font = "Arial 20")
+				# nextLevel: tuple of (text, Circle)
+			if nextLevel[1].isLongCir():
+				x, y = nextLevel[1].othercx, nextLevel[1].othercy-20
+			else:
+				x, y = nextLevel[1].cx, nextLevel[1].cy-20
+			canvas.create_text(x, y, text = nextLevel[0], font = "Arial 20")
 			data.levels.pop(0)
 
 
 
+####################################
+# gameOver mode
+####################################
 
+def gameOverMousePressed(event, data):
+	pass
+
+def gameOverMouseRelease(event, data):
+	pass
+
+def gameOverKeyPressed(event, data):
+	pass
+
+def gameOverKeyRelease(event, data):
+	pass
+
+def gameOverTimerFired(data):
+	pass
+
+def gameOverRedrawAll(canvas, data):
+	canvas.create_text(data.width/2, data.height/2,
+					   text="Your Score:\n%d" % data.score,
+					   font="Arial 26 bold")
 
 
 
 
 # run function taken from 15-112 course note "mode demo"
-# but i modified the keypress and keyrelease functions
+# but I modified the mouse and key functions
 # http://www.kosbie.net/cmu/fall-16/15-112/notes/notes-animations-examples.html
 
 def run(width=300, height=300):
@@ -267,6 +349,10 @@ def run(width=300, height=300):
 
 	def mousePressedWrapper(event, canvas, data):
 		mousePressed(event, data)
+		redrawAllWrapper(canvas, data)
+
+	def mouseReleaseWrapper(event, canvas, data):
+		mouseRelease(event, data)
 		redrawAllWrapper(canvas, data)
 
 	def keyPressedWrapper(event, canvas, data):
@@ -299,6 +385,8 @@ def run(width=300, height=300):
 	# set up events
 	root.bind("<Button-1>", lambda event:
 							mousePressedWrapper(event, canvas, data))
+	root.bind("<ButtonRelease-1>", lambda event:
+							mouseReleaseWrapper(event, canvas, data))
 	root.bind('<KeyPress>', lambda event:
 							keyPressedWrapper(event, canvas, data))
 	root.bind('<KeyRelease>', lambda event: 
